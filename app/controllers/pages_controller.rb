4ab0820c7bb51a3be32e6e463b82d9f83d48a02f
Louis Hocquet <louis.hocquet@sfr.fr>
2024-04-15 18:50:44 +0200

    Add contact anchor from last project card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,11 +9,5 @@ class PagesController < ApplicationController
   def wattivet_detail
     @project_title = "Wattivet"
     @project_url = "https://www.wattivet.fr"
-    @project_description = "
-      Wattivet est une clinique vétérinaire sur Wattignies, spécialisée dans les soins aux chiens et chats.
-      L'équipe souhaitait développer sa notoriété sur internet, nous avons donc conçu un site internet ensemble pour
-      partager aux clients :
-    "
-
   end
 end
