7b1ba904ef7235ff6252027552e2ff991edbc5df
Louis Hocquet <louis.hocquet@sfr.fr>
2024-04-15 18:32:27 +0200

    Add wattivet detail page + link to website

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,19 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :wattivet_detail]
   def home
   end
 
   def admin
+  end
+
+  def wattivet_detail
+    @project_title = "Wattivet"
+    @project_url = "https://www.wattivet.fr"
+    @project_description = "
+      Wattivet est une clinique vétérinaire sur Wattignies, spécialisée dans les soins aux chiens et chats.
+      L'équipe souhaitait développer sa notoriété sur internet, nous avons donc conçu un site internet ensemble pour
+      partager aux clients :
+    "
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  get "wattivet", to: "pages#wattivet_detail", as: :wattivet_detail
   # get "admin", to: "pages#admin", as: :admin
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
