e47e520dee8305625e3b0deffd48e03e0b6dc153
Louis Hocquet <louis.hocquet@sfr.fr>
2024-04-19 19:38:30 +0200

    removed form from home due to anchor issue on form submission

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,14 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :wattivet_detail]
   def home
-    @contact = Contact.new
-    if params[:contact_info]
-      @contact.name = params[:contact_info][:name] if params[:contact_info][:name]
-      @contact.email = params[:contact_info][:email] if params[:contact_info][:email]
-      @contact.message = params[:contact_info][:message] if params[:contact_info][:message]
-      @email_errors =  params[:contact_info][:errors][:email]
-      @message_errors =  params[:contact_info][:errors][:message]
-    end
+    # @contact = Contact.new
+    # if params[:contact_info]
+    #   @contact.name = params[:contact_info][:name] if params[:contact_info][:name]
+    #   @contact.email = params[:contact_info][:email] if params[:contact_info][:email]
+    #   @contact.message = params[:contact_info][:message] if params[:contact_info][:message]
+    #   @email_errors =  params[:contact_info][:errors][:email]
+    #   @message_errors =  params[:contact_info][:errors][:message]
+    # end
   end
 
   def admin
