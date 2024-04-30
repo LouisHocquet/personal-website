9f6e94dbdaaa6994918fac04a279ce0aab890a36
Louis Hocquet <louis.hocquet@sfr.fr>
2024-04-30 17:25:33 +0200

    Create expertises with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+p "Creating Expertises ..."
+p "---------------------"
+# Creating expertises
+unless  Expertise.where(name: "Product Management")
+  expertise = Expertise.new(
+    name: "Product Management",
+    fontawesome: "fa-solid fa-rocket"
+  )
+  Expertise.save!
+  p "Create #{expertise.name}"
+end
+
+unless  Expertise.where(name: "Développement Web")
+  expertise = Expertise.new(
+    name: "Développement Web",
+    fontawesome: "fa-solid fa-code"
+  )
+  Expertise.save!
+  p "Create #{expertise.name}"
+end
+
+unless  Expertise.where(name: "Design Web")
+  expertise = Expertise.new(
+    name: "Design Web",
+    fontawesome: "fa-solid fa-wand-magic-sparkles"
+  )
+  Expertise.save!
+  p "Create #{expertise.name}"
+end
+
+p "Seeds finished !"
